156af4128f2148af5a859c9c977244ab81ffece2
Zulma <zrmartinezg@gmail.com>
2022-02-07 18:48:32 -0500

    Solve bon-appetit challenge

---
+++ bon-appetit.js
@@ -0,0 +1,30 @@
+
+/*
+ * Complete the 'bonAppetit' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY bill
+ *  2. INTEGER k item that Anna did not eat.
+ *  3. INTEGER b the amount of money that Brian charged Anna for her share of the bill.
+ */
+
+function bonAppetit(bill, k, b) {
+  // Write your code here
+  let sum = 0;
+  let totalBillE = 0
+  for(let i = 0; i < bill.length; i++){
+    sum += bill[i];
+  }
+totalBillE = (sum - bill[k])/2
+
+if (b === totalBillE){
+  console.log("Bon Appetit")
+} else (
+  console.log(b - totalBillE)
+)
+}
+bonAppetit([3, 10, 2, 9], 1, 12)
+// answer 5
+
+bonAppetit([3, 10, 2, 9], 1, 7)
+// answer Bon Appetit
