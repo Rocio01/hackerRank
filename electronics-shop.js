ae9103c0f518cdf30f0e82f3a5299bff2a1ec4cd
Zulma Rocio <zrmartinezg@gmail.com>
2022-02-16 18:07:09 -0500

    Solve electronics shop in js

---
+++ electronics-shop.js
@@ -0,0 +1,30 @@
+// A person wants to determine the most expensive computer keyboard and USB
+//  drive that can be purchased with a give budget. Given price lists for keyboards
+//  and USB drives and a budget, find the cost to buy them. If it is not possible to buy
+//   both items, return -1.
+
+function getMoneySpent(b, keyboards, drives) {
+ let result = -1
+ 
+
+ for (let i = 0; i < keyboards.length; i++){
+   for(let j = 0; j < drives.length; j++){
+    let temp = 0;
+     if(keyboards[i] + drives[j] < b){
+       temp = keyboards[i] + drives[j];
+       result = temp > result ? temp : -1
+     }
+
+   }
+
+ }
+
+console.log(result);
+return result
+}
+
+getMoneySpent( 5, [4], [5] )
+// answer -1
+
+getMoneySpent( 10, [3, 1], [5, 2 , 8] )
+// 9
