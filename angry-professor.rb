ed78f778001ca9475a449087ab8eb7c97d42b04f
Zulma Rocio <zrmartinezg@gmail.com>
2022-02-24 20:57:13 -0500

    Add solution angry professor ruby

---
+++ angry-professor.rb
@@ -0,0 +1,27 @@
+# A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, 
+# the professor decides to cancel class if fewer than some number of students are present when class starts.
+
+#
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+
+def angryProfessor(k, a)
+  # Write your code here
+  arr = a.select {|i| i <= 0}
+
+  arr.length >= k ? result = "NO" : result = "YES"
+  print(result)
+  result
+  
+
+end
+
+angryProfessor(3, [-1, -3, 4, 2])
+# // YES
+angryProfessor(2, [0, -1, 2, 1])
+# // NO
